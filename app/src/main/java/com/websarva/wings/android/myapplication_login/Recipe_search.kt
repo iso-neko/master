d26c485e53fb6e36c8e910c0892022a13b37d9bd
iso-neko <takumi.iso357@gmail.com>
2021-01-22 11:15:17 +0900

    初保存

---
+++ app/src/main/java/com/websarva/wings/android/myapplication_login/Recipe_search.kt
@@ -0,0 +1,11 @@
+package com.websarva.wings.android.myapplication_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Recipe_search : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_search)
+    }
+}
