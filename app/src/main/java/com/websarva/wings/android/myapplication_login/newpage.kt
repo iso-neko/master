c103d7c5ed45649890de8bd5a12112ac7b43b2b6
iso-neko <takumi.iso357@gmail.com>
2021-01-22 11:21:35 +0900

    初保存

---
+++ app/src/main/java/com/websarva/wings/android/myapplication_login/newpage.kt
@@ -0,0 +1,11 @@
+package com.websarva.wings.android.myapplication_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class newpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_newpage)
+    }
+}
