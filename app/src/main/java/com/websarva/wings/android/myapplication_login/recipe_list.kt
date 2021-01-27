0e15a4eded0863b57377a0ae378d4012061ab7e4
iso-neko <takumi.iso357@gmail.com>
2021-01-27 11:57:47 +0900

    Isoテスト ver2

---
+++ app/src/main/java/com/websarva/wings/android/myapplication_login/recipe_list.kt
@@ -0,0 +1,11 @@
+package com.websarva.wings.android.myapplication_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recipe_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_list)
+    }
+}
