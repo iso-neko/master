e4e3efb1c5e6e3e5d86e29e9e5919f2005de5d83
iso-neko <takumi.iso357@gmail.com>
2021-01-28 11:49:45 +0900

    Isoテスト ver2

---
+++ app/src/main/java/com/websarva/wings/android/myapplication_login/chokubaizyo_search.kt
@@ -0,0 +1,11 @@
+package com.websarva.wings.android.myapplication_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class chokubaizyo_search : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chokubaizyo_search)
+    }
+}
---
+++ app/src/main/java/com/websarva/wings/android/myapplication_login/product_search.kt
@@ -0,0 +1,11 @@
+package com.websarva.wings.android.myapplication_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class product_search : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_search)
+    }
+}
